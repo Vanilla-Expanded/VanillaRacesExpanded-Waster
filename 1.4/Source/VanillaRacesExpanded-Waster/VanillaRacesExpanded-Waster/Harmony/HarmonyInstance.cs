6d35b056c430777058a895b5163b563b5bf8fa3a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-04-19 14:20:22 +0200

    Initial commit

---
+++ 1.4/Source/VanillaRacesExpanded-Waster/VanillaRacesExpanded-Waster/Harmony/HarmonyInstance.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace VanillaRacesExpandedWaster
+{
+
+    public class HarmonyInstance : Mod
+    {
+        public HarmonyInstance(ModContentPack content) : base(content)
+        {
+            harmonyInstance = new Harmony("OskarPotocki.VREWaster");
+            harmonyInstance.PatchAll();
+        }
+
+        public static Harmony harmonyInstance;
+    }
+
+}
